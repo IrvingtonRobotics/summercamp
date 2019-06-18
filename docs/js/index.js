f4113499953357b70600f2441c3c296e66f84f33
Jared Hughes <jahughes241@gmail.com>
2019-06-18 15:48:07 -0700

    Use Sergey (SSG) for shared html files in prep for multiple pages

---
+++ docs/js/index.js
@@ -0,0 +1,16 @@
+// enlargeable images
+$('img[data-enlargeable]').addClass('img-enlargeable').click(function(){
+	// https://stackoverflow.com/a/50430187
+	var src = $(this).attr('src');
+	$('<div>').css({
+		background: 'RGBA(0,0,0,0.8) url('+src+') no-repeat center',
+		backgroundSize: 'contain',
+		width:'100%', height:'100%',
+		position:'fixed',
+		zIndex:'10000',
+		top:'0', left:'0',
+		cursor: 'zoom-out'
+	}).click(function(){
+		$(this).remove();
+	}).appendTo('body');
+});
