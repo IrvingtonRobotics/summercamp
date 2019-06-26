b5ca2e1481cc81ea1eff12ff9c055d1a28cfefe5
Jared Hughes <jahughes241@gmail.com>
2019-06-26 00:12:59 -0700

    Add Genius Kids information

--- docs/js/index.js
+++ docs/js/index.js
@@ -14,3 +14,66 @@ $('img[data-enlargeable]').addClass('img-enlargeable').click(function(){
 		$(this).remove();
 	}).appendTo('body');
 });
+
+var chosenButtonClass = "is-info is-selected";
+var campNames = [ "nurture", "genius" ];
+
+function getButton(camp) {
+	return $("#choose-" + camp)
+}
+
+function chooseCamp(camp) {
+	$("body")
+		.removeClass("is-unchosen is-genius is-nurture")
+		.addClass("is-" + camp);
+	campNames.forEach(function(camp) {
+		getButton(camp).removeClass(chosenButtonClass);
+	})
+	getButton(camp)
+		.addClass(chosenButtonClass)
+		.off("click")
+		.click(unchooseCamp);
+}
+
+function unchooseCamp() {
+	$("body").removeClass("is-nurture is-genius").addClass("is-unchosen");
+	campNames.forEach(function(camp) {
+		getButton(camp)
+			.removeClass(chosenButtonClass)
+			.click(function() {
+				chooseCamp(camp)
+			})
+	})
+}
+
+unchooseCamp();
+
+if (window.location.hash == "#nurture") {
+	chooseCamp("nurture");
+} else if (window.location.hash == "#genius") {
+	chooseCamp("genius");
+}
+
+// https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
+$('a[href*="#"]').on('click', function(e) {
+  // e.preventDefault()
+
+	console.log($($(this).attr('href')).offset());
+
+	var href = $(this).attr('href');
+	var element = $(href);
+	if (element.length == 0) {
+		element = $("[name=" + href.substring(1) + "]")
+	}
+  scrollTo(element);
+})
+
+function scrollTo(element) {
+	$('html, body').animate(
+    {
+      scrollTop: $(element).offset().top,
+    },
+    500,
+    'linear'
+  )
+}
